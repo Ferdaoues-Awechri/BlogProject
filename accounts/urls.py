a88b0847a4f03da4a7ed014647d10ce4c94e4226
Ferdaoues-Awechri <aouachriferdaoues@gmail.com>
2025-11-11 15:15:18 +0100

    Implement user authentication features: add login, signup, and profile templates; enhance styling; and configure URLs for user management.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(template_name="registration/changepassword.html"), name='change_password'),
+    path('password-change/done/', TemplateView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import TemplateView
+from django.contrib import messages
+from django.shortcuts import redirect 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class ProfileView(PermissionRequiredMixin,LoginRequiredMixin, TemplateView, ):
+    template_name = 'registration/profile.html'
+    permission_required = 'auth.view_user'
+
+
+    # def test_func(self):
+    #     return self.request.user.is_superuser
+
+    # def handle_no_permission(self):
+    #     messages.error(self.request, "Access denied: Superuser only.")
+    #     return redirect('home')
+
